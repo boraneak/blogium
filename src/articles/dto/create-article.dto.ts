3218d3986b260c2fd110642953ca166817d4dc14
boraneak <boraneak123@gmail.com>
2023-07-12 20:32:36 +0700

    define create articles endpoint

--- src/articles/dto/create-article.dto.ts
+++ src/articles/dto/create-article.dto.ts
@@ -1,1 +1,13 @@
-export class CreateArticleDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { fail } from 'assert';
+
+export class CreateArticleDto {
+  @ApiProperty()
+  title: string;
+  @ApiProperty({ required: false })
+  description?: string;
+  @ApiProperty()
+  body: string;
+  @ApiProperty({ required: false, default: false })
+  published?: boolean = false;
+}
