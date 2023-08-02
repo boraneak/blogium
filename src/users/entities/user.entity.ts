d573d72ed5bb43233c761b6f9aa53d4716687b4c
boraneak <boraneak123@gmail.com>
2023-08-02 20:47:41 +0700

    define the User entity and DTO classes

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,21 @@
-export class User {}
+import { ApiProperty } from '@nestjs/swagger';
+import { User } from '@prisma/client';
+
+export class UserEntity implements User {
+  @ApiProperty()
+  id: number;
+
+  @ApiProperty()
+  createdAt: Date;
+
+  @ApiProperty()
+  updatedAt: Date;
+
+  @ApiProperty()
+  name: string;
+
+  @ApiProperty()
+  email: string;
+  // @ApiProperty() // password field is sensitive. you do not want to expose it in your API.
+  password: string;
+}
