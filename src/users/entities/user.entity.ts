42c7403a5652faf3b005b85474535af117e40e05
boraneak <boraneak123@gmail.com>
2023-08-08 20:50:29 +0700

    exclude password field from response body

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,7 +1,11 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { User } from '@prisma/client';
+import { Exclude } from 'class-transformer';
 
 export class UserEntity implements User {
+  constructor(partial: Partial<UserEntity>) {
+    Object.assign(this, partial);
+  }
   @ApiProperty()
   id: number;
 
@@ -16,6 +20,6 @@ export class UserEntity implements User {
 
   @ApiProperty()
   email: string;
-  // @ApiProperty() // password field is sensitive. you do not want to expose it in your API.
+  @Exclude()
   password: string;
 }
