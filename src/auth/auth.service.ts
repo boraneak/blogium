49f372335e09f446a5295fb7f9ec8c6f3cba2027
boraneak <boraneak123@gmail.com>
2023-08-16 21:31:43 +0700

    imple. /auth/login endpoint

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,31 @@
-import { Injectable } from '@nestjs/common';
-
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthEntity } from './entity/auth.entity';
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private prisma: PrismaService, private jwtService: JwtService) {}
+
+  async login(email: string, password: string): Promise<AuthEntity> {
+    // fetch the user with the given email
+    const user = await this.prisma.user.findUnique({ where: { email: email } });
+    // if no user is found, throw a party
+    if (!user) {
+      throw new NotFoundException(`no user found for email: ${email}`);
+    }
+    // check if the passoword is correct
+    const isPasswordValid = user.password === password;
+    // if password does not match, throw an error
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('invalid password');
+    }
+    // generate a JWT containing the user's ID and return it
+    return {
+      accessToken: this.jwtService.sign({ userId: user.id }),
+    };
+  }
+}
